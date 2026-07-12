3a26eee170920393c976fc7d108f5324e553b739
Anwar Hossain <mahedianwar@gmail.com>
2026-07-12 17:25:46 +0600

    feat: login page markup

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,1 +1,59 @@
-export default function LoginPage() { return <div>Login</div>; }
+import { Link } from 'react-router-dom';
+
+export default function LoginPage() {
+  return (
+    <section className="_social_login_wrapper _layout_main_wrapper">
+      <div className="_shape_one"><img src="/assets/images/shape1.svg" alt="" className="_shape_img" /></div>
+      <div className="_shape_two"><img src="/assets/images/shape2.svg" alt="" className="_shape_img" /></div>
+      <div className="_shape_three"><img src="/assets/images/shape3.svg" alt="" className="_shape_img" /></div>
+      <div className="_social_login_wrap">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-xl-8 col-lg-8 col-md-12 col-sm-12">
+              <div className="_social_login_left">
+                <div className="_social_login_left_image"><img src="/assets/images/login.png" alt="Login" className="_left_img" /></div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
+              <div className="_social_login_content">
+                <div className="_social_login_left_logo _mar_b28"><img src="/assets/images/logo.svg" alt="BuddyScript" className="_left_logo" /></div>
+                <p className="_social_login_content_para _mar_b8">Welcome back</p>
+                <h4 className="_social_login_content_title _titl4 _mar_b50">Login to your account</h4>
+                <form className="_social_login_form">
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label htmlFor="email" className="_social_login_label _mar_b8">Email</label>
+                        <input id="email" type="email" className="form-control _social_login_input" />
+                      </div>
+                    </div>
+                    <div className="col-12">
+                      <div className="_social_login_form_input _mar_b14">
+                        <label htmlFor="password" className="_social_login_label _mar_b8">Password</label>
+                        <input id="password" type="password" className="form-control _social_login_input" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-12">
+                      <div className="_social_login_form_btn _mar_t40 _mar_b60">
+                        <button type="button" className="_social_login_form_btn_link _btn1">Login now</button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                <div className="row">
+                  <div className="col-12">
+                    <div className="_social_login_bottom_txt">
+                      <p className="_social_login_bottom_txt_para">Don't have an account? <Link to="/register">Create New Account</Link></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
