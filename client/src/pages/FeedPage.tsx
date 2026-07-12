97ba6ee6a0f616b85e3a20a47d7344e6bb45bb71
Anwar Hossain <mahedianwar@gmail.com>
2026-07-12 20:46:01 +0600

    feat: match static feed layout

--- client/src/pages/FeedPage.tsx
+++ client/src/pages/FeedPage.tsx
@@ -1,25 +1,18 @@
-import CreatePostBox from '../components/CreatePostBox';
-import PostList from '../components/PostList';
-import { useAuth } from '../context/AuthContext';
+import FeedHeader from '../components/feed/FeedHeader';
+import FeedLeftSidebar from '../components/feed/FeedLeftSidebar';
+import FeedRightSidebar from '../components/feed/FeedRightSidebar';
+import StaticFeedCenter from '../components/feed/StaticFeedCenter';
 
 export default function FeedPage() {
-  const { user, logout } = useAuth();
   return (
     <div className="_layout _layout_main_wrapper">
-      <nav className="navbar _header_nav _padd_t10">
-        <div className="container _custom_container d-flex justify-content-between align-items-center">
-          <a className="navbar-brand" href="/feed"><img src="/assets/images/logo.svg" alt="BuddyScript" className="_nav_logo" /></a>
-          <div className="d-flex align-items-center gap-3">
-            <span>{user?.firstName} {user?.lastName}</span>
-            <button className="_btn1" onClick={() => logout()}>Logout</button>
-          </div>
-        </div>
-      </nav>
+      <FeedHeader />
       <div className="container _custom_container">
-        <div className="row justify-content-center">
-          <div className="col-xl-8 col-lg-10 col-md-12">
-            <CreatePostBox />
-            <PostList />
+        <div className="_layout_inner_wrap">
+          <div className="row">
+            <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12"><FeedLeftSidebar /></div>
+            <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12"><StaticFeedCenter /></div>
+            <div className="col-xl-3 col-lg-3 col-md-12 col-sm-12"><FeedRightSidebar /></div>
           </div>
         </div>
       </div>
